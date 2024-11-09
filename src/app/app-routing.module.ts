35a489cbd97ad0b3f86414c165e766658da3d6a3
Grupo2Duoc <riv.uribe@duocuc.cl>
2024-11-09 20:47:26 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,31 +5,31 @@ import { AuthGuard } from './auth.guard';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'login', // Redirección a la página de login
     pathMatch: 'full'
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule) // Lazy Loading en LoginPageModule
   },
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule),
-    canActivate: [AuthGuard]
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule), // Lazy Loading en HomePageModule
+    canActivate: [AuthGuard] // Protección de acceso con AuthGuard
   },
   {
     path: 'user-management',
-    loadChildren: () => import('./pages/user-management/user-management.module').then(m => m.UserManagementPageModule)
+    loadChildren: () => import('./pages/user-management/user-management.module').then(m => m.UserManagementPageModule) // Lazy Loading en UserManagementPageModule
   },
   {
     path: '**',
-    loadChildren: () => import('./pages/page404/page404.module').then(m => m.Page404PageModule)
+    loadChildren: () => import('./pages/page404/page404.module').then(m => m.Page404PageModule) // Lazy Loading en Page404PageModule para rutas no encontradas
   }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }) // Estrategia de precarga de todos los módulos después de la carga inicial
   ],
   exports: [RouterModule]
 })
